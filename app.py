208aa3a381ab1b74ef901b868e118f3ec34df9ff
Gabriel Vieira <gabrieldacruzvieira08@gmail.com>
2025-02-15 10:56:25 -0400

    add holerites

--- app.py
+++ app.py
@@ -2,7 +2,24 @@ import streamlit as st
 import PyPDF2
 import re
 
-def extract_employee_info(pdf_reader):
+def extract_holerite_info(pdf_reader):
+    employee_dict = {}
+    for page_number, page in enumerate(pdf_reader.pages, start=1):
+        text = page.extract_text()
+
+        matricula_match = re.search(r"CNPJ: CC:\s*(\d+)\s*Cód", text)
+
+        nome_match = re.search(r"(.+?)(?=\s*Nome do Funcionário)", text)
+
+        if matricula_match and nome_match:
+            matricula = matricula_match.group(1).strip()
+            nome = nome_match.group(1).strip()
+
+            employee_dict[matricula] = {"name": nome, "page": page_number}
+
+    return employee_dict
+
+def extract_ponto_info(pdf_reader):
     employee_dict = {}
     for page_number, page in enumerate(pdf_reader.pages, start=1):
         text = page.extract_text()
@@ -16,17 +33,43 @@ def extract_employee_info(pdf_reader):
             employee_dict[matricula] = {"name": name, "page": page_number}
     return employee_dict
 
-st.title("Visualizador de Folhas de Ponto")
+def detect_document_type(pdf_reader):
+    for page in pdf_reader.pages[:3]:
+        text = page.extract_text()
+        if re.search(r"CNPJ: CC:", text) and re.search(r"Cód", text):
+            return "holerite"
+        elif re.search(r"Matrícula", text) and re.search(r"Admissão", text):
+            return "ponto"
+    return "desconhecido"
+
+st.title("Visualizador de Folhas de Ponto e Holerites")
 
 uploaded_file = st.file_uploader("Carregue o arquivo PDF", type="pdf")
 
+if 'uploaded_file_path' not in st.session_state:
+    st.session_state.uploaded_file_path = None
+
 if uploaded_file:
-    if "dicionario" not in st.session_state:
+    if uploaded_file != st.session_state.uploaded_file_path:
+        st.session_state.dicionario = {}
+        
+        st.session_state.uploaded_file_path = uploaded_file
+    
         pdf_reader = PyPDF2.PdfReader(uploaded_file)
-        dicionario = extract_employee_info(pdf_reader)
+        
+        document_type = detect_document_type(pdf_reader)
+        
+        if document_type == "holerite":
+            dicionario = extract_holerite_info(pdf_reader)
+        elif document_type == "ponto":
+            dicionario = extract_ponto_info(pdf_reader)
+        else:
+            st.warning("Tipo de documento não identificado.")
+            dicionario = {}
+        
         st.session_state["dicionario"] = dicionario
     else:
-        dicionario = st.session_state["dicionario"]
+        dicionario = st.session_state.get("dicionario", {})
 
     if dicionario:
         matriculas_selecionadas = st.sidebar.multiselect(
